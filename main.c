efc1d01ae6e1a0cf11ab43bea1230e826b485b22
dreama <dimahocha2005@gmail.com>
2026-09-16 21:29:34 +0100

    player movement

--- main.c
+++ main.c
@@ -1,42 +1,94 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#define WINDOW_HEIGHT 1000
-#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 1500
+#define WINDOW_WIDTH 2000
+#define PADSPEED 5 // WARN: Yes, it is kinda bad to define this but I am way too lazy to do this properly
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-struct colour{
-    int r,g,b;
-};
+typedef struct colour{
+    unsigned char r,g,b;
+} colour;
+
+typedef struct vector2{
+    int x,y;
+} vector;
 //Not really a ball, more like a square
 struct pongBall{
-    int x,y,size;
-    struct colour colour;
+    vector pos,speed;
+    int size;
+    colour colour;
 };
 
 struct playerPad{
-    int x,y,height,width;
-    struct colour colour;
+    vector size,pos;
+    colour colour;
 };
+
+struct input{
+    char state;
+    SDL_Keycode key;
+};
+
+struct inputState{
+    char length;
+    struct input* array;
+};
+
 struct state{
     struct pongBall ball;
     struct playerPad p1Pad,p2Pad;
+    struct inputState inputs; 
 };
-struct state getState(void){
-    static const int padWidth = 30;
-    static struct state state = {
-    .ball = {.x = WINDOW_WIDTH/2, .y = WINDOW_HEIGHT,.size = 50},
-    .p1Pad = {.x = 0, .y = WINDOW_HEIGHT/2,.height=WINDOW_HEIGHT/2,.width=padWidth}}; // TODO:
-    return state;
+
+void drawRect(vector pos, vector size){
+    SDL_FRect rect = {.x = pos.x, .y = pos.y, .w = size.x, .h = size.y};
+    SDL_RenderRect(renderer,&rect);
+    SDL_RenderFillRect(renderer,&rect);
+}
+
+void drawPad(struct playerPad padData){
+    SDL_SetRenderDrawColor(renderer,padData.colour.r,padData.colour.g,padData.colour.b,SDL_ALPHA_OPAQUE);
+    drawRect(padData.pos,padData.size);
+}
+
+void drawBall(struct pongBall ballData){
+    SDL_SetRenderDrawColor(renderer,ballData.colour.r,ballData.colour.g,ballData.colour.b,SDL_ALPHA_OPAQUE);
+    vector rectSize = {ballData.size,ballData.size};
+    drawRect(ballData.pos,rectSize);
+}
+
+void setKeyState(struct inputState* inputState,char state,SDL_Keycode key){
+    if (inputState->length == 0) return ;
+    char count = 0;
+    do{
+        if ((inputState->array[count]).key == key){
+            (inputState->array[count]).state = state;
+        }
+    }while(count++<inputState->length);
+}
+
+char getKeyState(struct inputState* inputState,SDL_Keycode key){
+    if (inputState->length == 0) return 0;
+    char count = 0;
+    do{
+        if ((inputState->array[count]).key == key){
+            return (inputState->array[count]).state;
+        }
+    }while(count++<inputState->length);
+    return 0;
+}
+
+void movePad(struct playerPad* pad, int movement){
+    int newYPos = pad->pos.y + movement;
+    if (WINDOW_HEIGHT - pad->size.y > newYPos && newYPos > 0){
+        pad->pos.y = newYPos;
+    }
 }
 
 int init(void){
-    int padWidth = 30;
-    struct pongBall ball = {.x = WINDOW_WIDTH/2, .y = WINDOW_HEIGHT,.size = 50};
-    struct playerPad p1Pad = {.x = 0, .y = WINDOW_HEIGHT/2,.height=WINDOW_HEIGHT/2,.width=padWidth};
-    struct playerPad p2Pad = {.x = WINDOW_WIDTH-padWidth, .y = WINDOW_HEIGHT/2,.height=WINDOW_HEIGHT/2,.width=padWidth};
 
     SDL_SetAppMetadata("pong","1.0","com.game.pong");
 
@@ -54,13 +106,19 @@ int init(void){
     return 1;
 }
 
-char processEvents(void){
+char processEvents(struct state* gameState){
     SDL_Event event;
     while(SDL_PollEvent(&event)){
         switch(event.type){
             case SDL_EVENT_QUIT:
                 printf("INFO: got quit event\n");
                 return 0;
+                break;
+            case SDL_EVENT_KEY_DOWN:
+                setKeyState(&(gameState->inputs),1,event.key.key);
+                break;
+            case SDL_EVENT_KEY_UP:
+                setKeyState(&(gameState->inputs),0,event.key.key);
                 break;
             default:
                 break;
@@ -69,33 +127,74 @@ char processEvents(void){
     return 1;
 }
 
-char mainLoop(void){
-    const double time = ((double)SDL_GetTicks())/1000.0;
+void inputProcessing(struct state* gameState){
+    struct inputState inputState = gameState->inputs;
+    if (inputState.length == 0) return ;
+    char count = 0;
+    do{
+        if ((inputState.array[count]).state == 1){
+            switch ((inputState.array[count]).key){
+                case SDLK_A:
+                    movePad(&(gameState->p1Pad),-PADSPEED);
+                    break;
+                case SDLK_S:
+                    movePad(&(gameState->p1Pad), PADSPEED);
+                    break;
+                case SDLK_J:
+                    movePad(&(gameState->p2Pad),-PADSPEED);
+                    break;
+                case SDLK_K:
+                    movePad(&(gameState->p2Pad), PADSPEED);
+                    break;
+            }
+        }
+    }while(count++<inputState.length);
+}
+
+char mainLoop(struct state* gameState){
 
     SDL_SetRenderDrawColorFloat(renderer,0,0,0,SDL_ALPHA_OPAQUE);
 
     SDL_RenderClear(renderer);
+    
+    inputProcessing(gameState);
+
+    drawPad(gameState->p1Pad);
+    drawPad(gameState->p2Pad);
+    drawBall(gameState->ball);
 
     SDL_RenderPresent(renderer);
 
     return 0; // NOTE: This is the only place where 0 is SUCCESS (except exit statuses of course)
 }
 
-void onQuit(void){
+void onQuit(struct state* gameState){
     //clean up
+    
 }
 
 int main(void){
     printf("INFO: entrypoint reached!\n");
     if (!init()) return 1;
-    printf("INFO: initialisation completed successfully\n");
+    //initial state of the game
     char running = 1;
     char exception = 0;
+    char ballSize = 50;
+    vector padSize = {.x =30,.y=(int)WINDOW_HEIGHT/6};
+    colour gameColour = {255,255,255};
+    struct input inputs[] = {{.key = SDLK_A},{.key = SDLK_S},{.key = SDLK_J},{.key = SDLK_K}};
+    struct state gameState = {
+        .ball = {.pos = {.x = (WINDOW_WIDTH/2)-ballSize,.y = (WINDOW_HEIGHT/2)-ballSize},.size=ballSize,.colour = gameColour,.speed = {.x=10,.y=10}},
+        .p1Pad = {.size=padSize,.pos={.x=0,.y=(WINDOW_HEIGHT/2)-(padSize.y/2)},.colour = gameColour},
+        .p2Pad = {.size=padSize,.pos={.x=WINDOW_WIDTH-padSize.x,.y=(WINDOW_HEIGHT/2)-(padSize.y/2)},.colour = gameColour},
+        .inputs = {.length = sizeof(inputs)/sizeof(struct input),.array = inputs}
+    };
+    printf("INFO: initialisation completed successfully\n");
     while (running && !exception){
-        running = processEvents();
-        exception = mainLoop();
+        running = processEvents(&gameState);
+        exception = mainLoop(&gameState);
     }
-    onQuit();
+    onQuit(&gameState);
     return exception;
 }
 
